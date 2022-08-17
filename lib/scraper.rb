2687112cb284352b723e430f237be0594965c4ba
Ryan DeLong <rdelong111@gmail.com>
2022-08-17 17:26:21 -0500

    commit tree be5cdfc067f487e682c14edc9fb643d2b9a1ba10

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
